11af64ca65bfcc185c75a69c95760e02bdc525be
Rene G <r.gromeier@gmail.com>
2023-06-08 12:03:59 +0200

    feat: inital router and controller

---
+++ server/controller/index.controller.js
@@ -0,0 +1,48 @@
+const identifyPlant = async (req, res) => {
+	try {
+		res.status(200).send('indentification in progress');
+	} catch (error) {
+		res.status(500);
+		res.send('Error during identification', error);
+	}
+};
+
+const getGarden = async (req, res) => {
+	try {
+		res.status(200).send('Hello from Garden');
+	} catch (error) {
+		res.status(500);
+		res.send('Error finding garden', error);
+	}
+};
+
+const savePlantToGarden = async (req, res) => {
+	try {
+		res.status(201).send('planted in Garden');
+	} catch (error) {
+		res.status(400);
+		res.send(`Error planting plant: ${error}`);
+	}
+};
+
+const updatePlant = async (req, res) => {
+	const id = req.params.id;
+	try {
+		res.status(201).send(`plant ${id} visited`);
+	} catch (error) {
+		res.status(400);
+		res.send(`Error locating plant: ${error}`);
+	}
+};
+
+const removePlant = async (req, res) => {
+	const id = req.params.id;
+	try {
+		res.status(200).send(`Plant ${id} Removed`);
+	} catch (error) {
+		res.status(400);
+		res.send(`Error removing plant: ${error}`);
+	}
+};
+
+module.exports = {identifyPlant, getGarden, savePlantToGarden, updatePlant, removePlant};
--- server/router.js
+++ server/router.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const controller = require('./controller/index.controller');
 
 router.get('/', (req, res) => {
-	res.status(200).send(`Hello Server ${process.env.PORT}`);
+	res.status(200).send('Welcome to my Garden');
 });
+router.post('/identify', controller.identifyPlant);
+router.get('/garden', controller.getGarden);
+router.post('/garden', controller.savePlantToGarden);
+router.put('/garden/:id', controller.updatePlant);
+router.delete('/garden/:id', controller.removePlant);
 
 module.exports = router;
