123ab4d46e63565e7c72673f53ccb7d2c00ce7e7
Chris Scheppele <cmscheppele@gmail.com>
2023-06-16 10:39:16 +0200

    app working with store.ts now, set store to export default

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,9 +1,12 @@
 import {legacy_createStore as createStore, applyMiddleware, CombinedState} from 'redux';
 import rootReducer from './reducer';
 import { composeWithDevTools } from '@redux-devtools/extension'
+// /<reference lib="dom"/>
 
-export const store = createStore(rootReducer, composeWithDevTools && composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools && composeWithDevTools());
 
 export type RootState = ReturnType<typeof store.getState>
 
 export type appDispatch = typeof store.dispatch;
+
+export default store;
