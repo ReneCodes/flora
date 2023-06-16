098d4a95a8a7c0d4e0cd6e6af7ffb4ccb43fdad5
Juliana <jusoffiati@gmail.com>
2023-06-16 11:38:29 +0200

    ts action types

---
+++ client/src/Types.ts
@@ -0,0 +1,24 @@
+export interface Route {
+
+}
+
+export interface Plant {
+
+}
+
+export interface Idx {
+
+}
+
+export interface Text {
+
+}
+
+export interface Note {
+
+}
+
+export interface Garden {
+
+}
+
