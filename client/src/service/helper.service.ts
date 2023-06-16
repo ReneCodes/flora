78fbc271d0e257847e555b79a381f6abca88ffd2
Chris Scheppele <cmscheppele@gmail.com>
2023-06-16 11:09:08 +0200

    started ts conversion, paused to look into argument types

--- client/src/service/helper.service.ts
+++ client/src/service/helper.service.ts
@@ -1,11 +1,11 @@
 import {savePlant} from './APIClient';
 
-export const waterDrops = {
+export const waterDrops:{1: string[], 2: string[], 3: string[]} = {
 	1: ['💧', '10-14 days'],
 	2: ['💧💧', '5-7 days'],
 	3: ['💧💧💧', '2-3 days'],
 };
-export const wateringInfo = {
+export const wateringInfo: {1: string, 2: string, 3: string} = {
 	1: `For this plant, ensure the soil dries out between watering to avoid stagnant water and root rot. Water once or twice during winter dormancy, and more frequently during active growth.`,
 	2: `This plant is somewhat drought-tolerant, so don't worry if it dries out a bit. Brown fronds at the base indicate overwatering, while browning or wilting tips mean it needs more water.`,
 	3: `Plants that need a lot of water also need excess water to run out somewhere. Always make sure that your plant is planted in a pot with holes.`,
