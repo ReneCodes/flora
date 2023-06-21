b3a423604c93f78a3e6d4f12af232b29f0a9e470
Chris Scheppele <cmscheppele@gmail.com>
2023-06-21 15:08:56 +0200

    db post testing and db get testing implemented

--- server/src/Tests/test.ts
+++ server/src/Tests/test.ts
@@ -14,12 +14,25 @@ describe("Integration tests", () => {
 
   it('should save a plant to the database', async (): Promise<void> => {
     const plant1 = Mocks.plant1;
-    console.log('Plant to plant ==> ', plant1.plant_name)
     const res = await supertest(app).post('/garden')
       .send(plant1)
 
     expect(res.status).toStrictEqual(201);
     expect(res.body).toStrictEqual({ result: 'planted in Garden' });
+  })
+
+  it('should fetch plants from the database', async (): Promise<void> => {
+    await supertest(app).post('/garden').send(Mocks.plant1)
+    await supertest(app).post('/garden').send(Mocks.plant2)
+    await supertest(app).post('/garden').send(Mocks.plant3)
+    
+    const res = await supertest(app).get("/garden");
+    console.log("res.body: ", res.body);
+    expect(res.status).toStrictEqual(200);
+    expect(res.body.length).toBe(3);
+    expect(res.body[0].plant_name).toBe("Phlebodium aureum");
+    expect(res.body[1].plant_name).toBe("Oxalis triangularis");
+    expect(res.body[2].plant_name).toBe("Dracaena trifasciata");
   })
 
 })
