800ecdf587643035f6f1a65e95f3c10511e8b412
Rene G <r.gromeier@gmail.com>
2023-07-07 23:05:31 +0200

    Chore: clean up BE

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({limit: '5mb'}));
 app.use(router);
 
 app.listen(PORT, () => {
